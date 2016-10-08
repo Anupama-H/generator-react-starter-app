83901558fe4895e605b95d34a608d56817fcdbfd
Ravi Kumar Hamsa <ravikumar.anantha@yahoo.in>
2016-10-08 17:02:50 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-react-starter-app [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> Generator for Generating App to use with react-starter-app
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-react-starter-app using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-react-starter-app
+```
+
+Then generate your new project:
+
+```bash
+yo react-starter-app
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [Ravi Kumar Hamsa]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-react-starter-app.svg
+[npm-url]: https://npmjs.org/package/generator-react-starter-app
+[travis-image]: https://travis-ci.org/ravihamsa/generator-react-starter-app.svg?branch=master
+[travis-url]: https://travis-ci.org/ravihamsa/generator-react-starter-app
+[daviddm-image]: https://david-dm.org/ravihamsa/generator-react-starter-app.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/ravihamsa/generator-react-starter-app
---
+++ generators/app/index.js
@@ -0,0 +1,65 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.Base.extend({
+  prompting: function () {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the splendid ' + chalk.red('generator-react-starter-app') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'someAnswer',
+      message: 'Would you like to enable this option?',
+      default: true
+    }];
+
+    return this.prompt(prompts).then(function (props) {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    }.bind(this));
+  },
+
+  writing: function () {
+    this.fs.copy(
+      this.templatePath('dummyfile.txt'),
+      this.destinationPath('dummyfile.txt')
+    );
+    this.fs.copy(
+      this.templatePath('.gitignore'),
+      this.destinationPath('.gitignore')
+    );
+
+    this.fs.copy(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json')
+    );
+
+    this.fs.copy(
+      this.templatePath('webpack.config.js'),
+      this.destinationPath('webpack.config.js')
+    );
+
+    this.fs.copy(
+      this.templatePath('app.js'),
+      this.destinationPath('app.js')
+    );
+
+    this.fs.copy(
+      this.templatePath('src'),
+      this.destinationPath('src')
+    );
+
+    this.fs.copy(
+      this.templatePath('public'),
+      this.destinationPath('public')
+    );
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
