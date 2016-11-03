7a8e5aa63fef953b1595e76466461d2c7c668f8f
Ravi Kumar Hamsa <ravikumar.anantha@yahoo.in>
2016-11-03 11:55:33 +0530

    fixed missing dependency

---
+++ generators/app/templates/src/components/simpleForm/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by ravi.hamsa on 6/29/16.
+ */
+
+import SimpleCheckbox from './SimpleCheckbox'
+import SimpleElement from './SimpleElement'
+import SimpleSelect from './SimpleSelect'
+import SimpleInput from './SimpleInput'
+import SimpleTextArea from './SimpleTextArea'
+import {core, components} from 'react-starter-components';
+const { Form, FormElement } = components.Form;
+
+export default{
+    SimpleCheckbox,
+    SimpleElement,
+    SimpleSelect,
+    SimpleInput,
+    SimpleTextArea,
+    Form,
+    FormElement
+}
