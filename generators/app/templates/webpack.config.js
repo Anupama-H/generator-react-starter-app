cbb60b20a91e2488fe889dae0f67cee25c738193
Ravi Kumar Hamsa <ravikumar.anantha@yahoo.in>
2017-03-05 06:39:49 +0530

    updates to web pack config

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -4,31 +4,37 @@
 
 var webpack = require('webpack');
 var path = require('path');
+var WatchIgnorePlugin = require("webpack").WatchIgnorePlugin;
 
 module.exports = {
-    devtool:'source-map',
-    entry:['babel-polyfill','./src/main'],
-    output:{
-        path:path.join(__dirname, 'public'),
-        filename:'bundle.js'
-    },
-    resolve:{
-        extensions:['.js']
-    },
-    externals:{
-        'react':'React',
-        'react-dom':'ReactDOM'
-    },
-    module:{
-        loaders:[
-            {
-                test:/\.js?$/,
-                loader:'babel-loader',
-                query:{
-                    presets:['es2015', 'react', 'stage-0']
-                },
-                exclude:/node_modules/
-            }
-        ]
-    }
+  devtool:'source-map',
+  entry:['babel-polyfill','./src/main'],
+  output:{
+    path:path.join(__dirname, 'public'),
+    filename:'bundle.js'
+  },
+  resolve:{
+    extensions:['.js']
+  },
+  externals:{
+    'react':'React',
+    'react-dom':'ReactDOM'
+  },
+  module:{
+    loaders:[
+      {
+        test:/\.js?$/,
+        loader:'babel-loader',
+        query:{
+          presets:['es2015', 'react', 'stage-0']
+        },
+        exclude:/node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new WatchIgnorePlugin([
+      path.resolve(__dirname, './node_modules/'),
+    ]),
+  ]
 }
